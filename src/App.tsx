812292b553fd8c708df3e958c12656bdcd4fbca9
supertgo <thiagoroberto2003@gmail.com>
2021-06-21 14:11:03 -0300

    Aula 1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/Button'
+function App() {
+  return (
+    <div>
+      <Button text="botao 1"/>
+      <Button text="botao 2"/>
+    </div>
+  );
+}
+
+export default App;
