6952e785ebd865b879df0c9de3e14badac795117
supertgo <thiagoroberto2003@gmail.com>
2021-06-22 14:28:15 -0300

    Aula 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Button } from './components/Button'
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+import "./styles/global.scss";
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 function App() {
   return (
-    <div>
-      <Button text="botao 1"/>
-      <Button text="botao 2"/>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+//import { useAuth } from "../hooks/useAuth";
+
+export function NewRoom() {
+  //const { user } = useAuth();
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas Q&amp;A ao-vivo</strong>
+        <p>Tire dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova Sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar na sala</Button>
+          </form>
+          <p>
+            Quer entrar em um sala existente? <Link to="/">Clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
