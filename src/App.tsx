ffce34639ae78b92c72afdba77e06c8943105b23
supertgo <thiagoroberto2003@gmail.com>
2021-06-23 17:16:30 -0300

    Aula 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
+import { Room } from "./pages/Room";
 import "./styles/global.scss";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
